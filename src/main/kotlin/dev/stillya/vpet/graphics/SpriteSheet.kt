47054d372f932ce3246db01407e81eecadc74df8
stillya <st.ilya.101@gmail.com>
2026-02-19 22:07:49 +0500

    refactor: polish api a little bit

--- src/main/kotlin/dev/stillya/vpet/graphics/SpriteSheet.kt
+++ src/main/kotlin/dev/stillya/vpet/graphics/SpriteSheet.kt
@@ -1,9 +1,20 @@
 package dev.stillya.vpet.graphics
 
 import dev.stillya.vpet.config.AtlasFrame
+import dev.stillya.vpet.config.SpriteSheetAtlas
 import java.awt.Image
 
 data class SpriteSheet(
 	val image: Image,
 	val frames: List<AtlasFrame>
 )
+
+fun SpriteSheetAtlas.create(img: Image, animationTag: String): SpriteSheet {
+	val frameTag = this.meta.frameTags.find { it.name == animationTag }
+		?: throw IllegalArgumentException("Animation tag not found: $animationTag")
+
+	return SpriteSheet(
+		image = img,
+		frames = this.frames.subList(frameTag.from, frameTag.to + 1)
+	)
+}
